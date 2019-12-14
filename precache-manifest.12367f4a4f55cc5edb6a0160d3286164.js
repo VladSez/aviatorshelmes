21ff952dec26d483882554e447516196a56355ad
Vlad Sazon <smallredeagle@gmail.com>
2019-12-14 13:09:05 +0300

    Updates

---
+++ precache-manifest.12367f4a4f55cc5edb6a0160d3286164.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ff75ffec89739cd496ae6f56c22ed4",
+    "url": "/aviatorshelmes/index.html"
+  },
+  {
+    "revision": "797883a00ec83321297b",
+    "url": "/aviatorshelmes/static/css/main.d15649da.chunk.css"
+  },
+  {
+    "revision": "d0c1782ce685139c9ec2",
+    "url": "/aviatorshelmes/static/js/2.173f6eee.chunk.js"
+  },
+  {
+    "revision": "797883a00ec83321297b",
+    "url": "/aviatorshelmes/static/js/main.8cd515b9.chunk.js"
+  },
+  {
+    "revision": "3ec5879834d578924b9c",
+    "url": "/aviatorshelmes/static/js/runtime-main.b45614a9.js"
+  },
+  {
+    "revision": "a7f1d5d3db6a3f15c1aa301c52c65de9",
+    "url": "/aviatorshelmes/static/media/1.a7f1d5d3.png"
+  },
+  {
+    "revision": "fa64f32b09f3906e2d93039b3685f783",
+    "url": "/aviatorshelmes/static/media/10.fa64f32b.png"
+  },
+  {
+    "revision": "bd0ba8dbcf8f957fed62f3eab17da65b",
+    "url": "/aviatorshelmes/static/media/2.bd0ba8db.png"
+  },
+  {
+    "revision": "4ed5f8739347059fdaa55c886f3ff9ea",
+    "url": "/aviatorshelmes/static/media/3.4ed5f873.png"
+  },
+  {
+    "revision": "ac37d068b8a4ab8329a4dae918ba417c",
+    "url": "/aviatorshelmes/static/media/4.ac37d068.png"
+  },
+  {
+    "revision": "4c19f18b8866d45e77ed47f6b37ffd0d",
+    "url": "/aviatorshelmes/static/media/5.4c19f18b.png"
+  },
+  {
+    "revision": "6e1ed6520644e83507a5306601fa31e3",
+    "url": "/aviatorshelmes/static/media/6.6e1ed652.png"
+  },
+  {
+    "revision": "c557255e8efcb36c0842400187b8efc3",
+    "url": "/aviatorshelmes/static/media/7.c557255e.png"
+  },
+  {
+    "revision": "eb460f3c595d49d428075120a91d0807",
+    "url": "/aviatorshelmes/static/media/8.eb460f3c.png"
+  },
+  {
+    "revision": "c6171f97c6b79ba8849b0bce0bf520f7",
+    "url": "/aviatorshelmes/static/media/9.c6171f97.png"
+  },
+  {
+    "revision": "4e85d39cdaa4e72e10d2a92849ebd572",
+    "url": "/aviatorshelmes/static/media/Stolzl-Regular.4e85d39c.otf"
+  },
+  {
+    "revision": "9ed4866783cb12d4072599d468df568c",
+    "url": "/aviatorshelmes/static/media/background.9ed48667.png"
+  },
+  {
+    "revision": "95f19dafefdc299a32b8e414941ebb4d",
+    "url": "/aviatorshelmes/static/media/elements@1X.95f19daf.png"
+  },
+  {
+    "revision": "8e5e500a475ff67b284084f8ce6aae57",
+    "url": "/aviatorshelmes/static/media/final_backpng.8e5e500a.png"
+  },
+  {
+    "revision": "c9139d95c4bbbdc8a6323037e399f0fd",
+    "url": "/aviatorshelmes/static/media/logo.c9139d95.png"
+  },
+  {
+    "revision": "9e340471af6ba8ade578707cdc956515",
+    "url": "/aviatorshelmes/static/media/main_logo.9e340471.png"
+  },
+  {
+    "revision": "2b483a805292ea53b28586d716f604bc",
+    "url": "/aviatorshelmes/static/media/plane.2b483a80.png"
+  }
+]);
